6c2ed1f9dc63e5b2905c54b5f1b3275ef871b681
SmallZombie <SmallZombie@avgt.top>
2026-05-13 01:09:04 +0800

    fix: Blue Archive birthday

--- src/ba-birthday/WikiController.ts
+++ src/ba-birthday/WikiController.ts
@@ -1,93 +1,56 @@
-import { load } from 'cheerio';
 import { CharacterType } from './type/CharacterType.ts';
 import { CharacterDetailType } from './type/CharacterDetailType.ts';
 
 
 async function getAllCharacters(): Promise<CharacterType[]> {
-    const res = await fetch('https://www.gamekee.com/ba').then(res => res.text());
-    const $ = load(res);
+    // 2026.5.13: gamekee 加 edgeone 了，换个 wiki，让我们感谢鸡窝托斯古书馆
+    const res = await fetch('https://api.kivo.wiki/api/v1/data/students/?page=1&page_size=500&is_npc=false&is_install=true').then(res => res.json()) as {
+        data: {
+            max_page: number,
+            students: [{
+                id: number,
+                skin: string,
+                family_name: string,
+                given_name: string,
+                family_name_jp: string,
+                given_name_jp: string,
+            }]
+        }
+    };
 
-    const dataURL = $('script[src^="/ssr-vuex-store-state.js"]').attr('src')!;
-    const dataRes = await fetch('https://www.gamekee.com' + dataURL).then(res => res.text());
-    const tempData = JSON.parse(dataRes.slice('window.__INITIAL_STATE__ = '.length)) as {
-        ssrComponentData: {
-            componentName: string;
-            componentData: string;
-        }[];
-    }
-    const tempData2 = JSON.parse(tempData.ssrComponentData.find(v => v.componentName === 'wikiHome')!.componentData) as {
-        entryList: {
-            id: number;
-            child: {
-                id: number;
-                child: {
-                    name: string;
-                    content_id: number;
-                }[];
-            }[];
-        }[];
-    }
+    return res.data.students.filter(v => {
+        // 过滤掉皮肤
+        if (v.skin) return false;
 
-    const characters = tempData2.entryList.find(v => v.id === 23941)!.child.find(v => v.id === 49443)!.child;
-    return characters.filter(v => !v.name.endsWith('）')).map(v => ({
-        id: v.content_id,
-        name: v.name
+        // 过滤掉没有生日的
+        return ![
+            // 食蜂 操祈
+            316,
+            // 佐天 泪子
+            317,
+        ].includes(v.id);
+    }).map(v => ({
+        id: v.id,
+        name: `${v.family_name_jp} ${v.given_name_jp} (${v.family_name ? `${v.family_name} ${v.given_name}` : v.given_name})`,
     }));
 }
 
 async function getCharacterDetail(id: number): Promise<CharacterDetailType> {
-    const res = await fetch(`https://www.gamekee.com/v1/content/detail/${id}`, {
-        headers: {
-            'game-alias': 'ba'
-        }
-    }).then(res => res.json()) as {
+    const res = await fetch(`https://api.kivo.wiki/api/v1/data/students/${id}`,).then(res => res.json()) as {
         data: {
-            content_json: string
+            family_name: string,
+            given_name: string,
+            birthday: string, // 05-16
+            release_date: string, // 2021-02-04
         }
     }
-    const contentJson = JSON.parse(res.data.content_json) as {
-        type: 'ba',
-        baseData: [
-            [
-                // 第 5 个数组
-                {
-                    key: "AzbynVpi",
-                    type: "text",
-                    value: "实装日期"
-                },
-                {
-                    key: "mYnLOqb7",
-                    type: "text",
-                    value: string // 2021/02/04
-                },
-                // 第 245 个数组
-                {
-                    key: "0WaIu2PO",
-                    type: "text",
-                    value: "生日"
-                },
-                {
-                    key: "dOaYgEVF",
-                    type: "text",
-                    value: string // 5月16日
-                }
-            ]
-        ]
-    }
 
-    const baseData = contentJson.baseData.flat(1);
+    console.log(res.data.family_name, res.data.given_name);
 
-    // "2月19日"
-    const birthdayStr = baseData.find(v => v.key === 'dOaYgEVF')?.value!;
-    const birthdayStr2 = birthdayStr.replace('月', '/').replace('日', '');
-
-    // "2021/02/04"
-    const releaseStr = baseData.find(v => v.key === 'mYnLOqb7')?.value!;
-
-    // 有些时候会有编辑中的角色，并且 getAll 中是无法辨别的，只能在这里返回空值了
+    const birthdayText = res.data.birthday.replace('-', '/');
     return {
-        birthday: birthdayStr2 ? new Date(birthdayStr2 + ' UTC+0800') : void 0,
-        release: releaseStr ? new Date(releaseStr + ' UTC+0800') : void 0
+        birthday: new Date(`${birthdayText} UTC+0800`),
+        release: new Date(`${res.data.release_date} UTC+0800`),
     }
 }
 
